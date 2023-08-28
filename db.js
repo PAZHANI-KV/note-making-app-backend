4a297fbbc12b3a7f848f8483a81006e3a2ac3ec6
PAZHANI-KV <pazhanivel11111@gmail.com>
2023-08-28 12:02:10 +0530

    c

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 // const mongoURI = 'your-mongodb-atlas-connection-string';
 // const connection = mongoose.connect(process.env.mongoUrl);
-mongoose.connect("mongodb://Pazahani:Bluegreen@ac-ohcr8rq-shard-00-00.hyatial.mongodb.net:27017,ac-ohcr8rq-shard-00-01.hyatial.mongodb.net:27017,ac-ohcr8rq-shard-00-02.hyatial.mongodb.net:27017/noteapp?ssl=true&replicaSet=atlas-95uguv-shard-0&authSource=admin&retryWrites=true&w=majority", {
+mongoose.connect("mongodb://Pazahani:Bluegreen@ac-ohcr8rq-shard-00-00.hyatial.mongodb.net:27017,ac-ohcr8rq-shard-00-01.hyatial.mongodb.net:27017,ac-ohcr8rq-shard-00-02.hyatial.mongodb.net:27017/?ssl=true&replicaSet=atlas-95uguv-shard-0&authSource=admin&retryWrites=true&w=majority", {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
